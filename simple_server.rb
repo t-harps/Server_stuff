f9ebbf35e7164c2e49d047d957db359a5aa76cf2
t-harps <t.w.harps@gmail.com>
2015-07-08 15:49:31 +0100

    Initial

--- simple_server.rb
+++ simple_server.rb
@@ -1,21 +1,38 @@
 require 'socket'
+require 'json'
+
 
 server = TCPServer.open(2000)
 
 loop {
 	client = server.accept
+  
+  header = ''
+  while line = client.gets
+    header << line
+    break if header =~ /\r\n\r\n$/
+  end
 
-	request = client.gets
-	request_header, request_body = request.split("\r\n\r\n", 2)
-	req = request_header.split(" ")
+	req = header.split
 	verb = req[0]
 	path = req[1][1..-1]
+
 	if File.exist?(path)
+		client.puts "HTTP/1.0 200 OK\r\n\r\n"
+		contents = File.read(path)
 		case verb
 			when "GET"
-				contents = File.read(path)
-				client.puts "HTTP/1.0 200 OK\r\n\r\n"
 				client.puts contents
+			when "POST"
+				body = ""
+    		while line = client.gets
+      		body << line
+      		break if body =~ /\r\n\r\n$/
+    		end
+    		body = body.chomp.chomp
+				params = JSON.parse(body)
+				user_data = "<li>name: #{params['person']['name']}</li><li>e-mail: #{params['person']['email']}</li>"
+        client.puts contents.gsub('<%= yield %>', user_data)
 			else
 				puts "not sure what's going on"	
 		end
--- tiny_web_browser.rb
+++ tiny_web_browser.rb
@@ -1,9 +1,27 @@
 require 'socket'
+require 'json'
 
 host = 'localhost'
 port = 2000
+params = Hash.new { |hash, key| hash[key] = Hash.new } 
 
-request = "GET /index.html HTTP/1.0\r\n\r\n"
+puts "What kind of request would you like to send?"
+verb = gets.chomp
+
+if verb == "GET"
+	request = "GET /index.html HTTP/1.0\r\n\r\n"
+elsif verb == "POST"
+	puts "Name?"
+	name = gets.chomp
+	puts "Email?"
+	email = gets.chomp
+	params[:person][:name] = name
+    params[:person][:email] = email
+    body = params.to_json
+
+	request = "POST /thanks.html HTTP/1.0\r\nContent-Length: #{body.length}\r\n\r\n#{body}\r\n\r\n"
+else
+end
 
 socket = TCPSocket.open(host,port)
 socket.print(request)
